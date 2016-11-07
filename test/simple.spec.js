a14efe23ba6c928fa44a4a6954addaa03e0b17bd
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-11-07 18:20:40 +0100

    also do determinism checks in simple call

--- test/simple.spec.js
+++ test/simple.spec.js
@@ -2,17 +2,20 @@ const asyncForEach = require('../')
 const test = require('tape')
 
 test('returns asynchronously and array of objects and no error', function (t) {
-  t.plan(3)
+  t.plan(5)
   const arr = []
+  let count = 2
 
   arr.push(function one (task, index, array, cb) {
     process.nextTick(() => {
+      t.equal(--count, 1)
       return cb(null, {'one': 'one'})
     })
   })
 
   arr.push(function two (task, index, array, cb) {
     setTimeout(function () {
+      t.equal(--count, 0)
       return cb(null, {'two': 'two'})
     }, 1000)
   })
